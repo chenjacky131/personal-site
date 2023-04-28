55f0b1ea77233ef0e874102b73e9e3d3c2e31388
ccw <chencw@cniship.com>
2023-04-28 15:59:41 +0800

    Revert "docs(导航站栏): 增加Jenkins部署地址"
    This reverts commit 2f104e65e78a35b2d445badfc0b01033984c5347.

--- src/components/HeaderComponent/HeaderComponent.vue
+++ src/components/HeaderComponent/HeaderComponent.vue
@@ -73,18 +73,6 @@
           博客
         </a>
       </a-menu-item>
-      <a-menu-item key="jenkins">
-        <template #icon>
-          <cloud-upload-outlined />
-        </template>
-        <a
-          href="http://116.205.172.255:8080/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Jenkins
-        </a>
-      </a-menu-item>
     </a-menu>
   </div>
 </template>
@@ -96,7 +84,6 @@ import {
   SendOutlined,
   HomeOutlined,
   AppstoreOutlined,
-  CloudUploadOutlined
 } from "@ant-design/icons-vue";
 import { RouterLink, useRoute } from "vue-router";
 import logo from "@/assets/img/logo.png";
